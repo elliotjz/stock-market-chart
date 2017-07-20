a09485447194a9bccadaf5a029a17b2ba501ca41
Elliot Zoerner <eliot.zoerner@gmail.com>
2017-07-20 21:57:13 +0200

    charting multiple stocks

--- public/js/chart.js
+++ public/js/chart.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
     function getHistoricalData(stockCode, callback) {
 
+        console.log("getting historical data for " + stockCode);
+
         let baseUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=';
         let keyUrl = '&apikey=26BIR4PKMC1G6V0A';
 
@@ -11,47 +13,65 @@ $(document).ready(function() {
             if (!data || !data['Weekly Time Series']) {
                 err = true;
             }
+            console.log('data for ' + stockCode + ":");
+            console.log(data);
             callback(err, !err && data['Weekly Time Series']);
         });
     }
 
-    chartStocks(['GOOG', 'MSFT']);
-
     function chartStocks(stocks) {
-
+        console.log("charting " + stocks);
+        let promises = [];
+        for (let i = 0; i < stocks.length; i++) {
+            let p = new Promise(function(resolve, reject) {
+                getHistoricalData(stocks[i], function(err, data) {
+                    if (err) reject(err);
+                    resolve(data);
+                })
+            })
+            promises.push(p);
+            console.log("promise created for " + stocks[i]);
+        }
+        Promise.all(promises).then(historicalDataArray => {
+            console.log("Collected nececary information. Stock price data:");
+            console.log(historicalDataArray);
+            putItOnAChart(stocks, historicalDataArray);
+        })
     }
-    
-    getHistoricalData('FBR.AX', function(err, data) {
 
-        let date = new Date();
-        let fiveYearsAgo = date.setYear(date.getYear() + 1900 - 5);
-        
-        let fiveYearData = {};
-        for (item in data) {
-            let dataDate = new Date(item);
-            if (dataDate > fiveYearsAgo) {
-                fiveYearData[item.split(' ')[0]] = data[item];
+
+    chartStocks(['AMZN', 'FB']);
+
+    function putItOnAChart(stocks, historicalDataArray) {
+
+        let datasets = [];
+        let labels = [];
+        for (let i = 0; i < stocks.length; i++) {
+
+            let parsedData = parseData(historicalDataArray[i]);
+            let keys = Object.keys(parsedData).reverse();
+            let values = Object.values(parsedData).reverse();
+            
+            if (keys.length > labels.length) {
+                labels = keys;
             }
-        }
 
-        let keys = Object.keys(fiveYearData);
-        let values = Object.values(fiveYearData).reverse;
-        let closePrices = [];
-        for (let i = 0; i < values.length; i++) {
-            closePrices.push(values[i]["4. close"]);
-        }
+            let data = {
+                label: stocks[i],
+                backgroundColor: 'rgba(0,0,0,0)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: values
+            }
 
+            datasets.push(data);
+        }
+        
         // create chart
         let ctx = document.getElementById('my-chart').getContext('2d');
         
         let myData = {
-            labels: keys,
-            datasets: [{
-                label: "APPL",
-                backgroundColor: 'rgba(0,0,0,0)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: closePrices,
-            }]
+            labels: labels,
+            datasets: datasets
         }
 
         let myLineChart = new Chart(ctx, {
@@ -59,5 +79,23 @@ $(document).ready(function() {
             data: myData,
             options: {}
         });
-    });
+    }
+
+    function parseData(fullData) {
+
+        let date = new Date();
+        let fiveYearsAgo = date.setYear(date.getYear() + 1900 - 5);
+        
+        let parsedData = {};
+        for (key in fullData) {
+            let dataDate = new Date(key);
+            if (dataDate > fiveYearsAgo) {
+                parsedData[key.split(' ')[0]] = fullData[key]['4. close'];
+            }
+        }
+
+        console.log("parsed data:");
+        console.log(parsedData);
+        return parsedData;
+    }
 })
